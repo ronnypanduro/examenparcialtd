b7a6898fe02faf6ffed5d8dec69346c5a30ada71
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2024-11-13 20:26:19 -0500

    subiendo proyecto inicial

---
+++ examenparcial/src/main/java/com/examenparcial/examenparcial/ExamenparcialApplication.java
@@ -0,0 +1,13 @@
+package com.examenparcial.examenparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenparcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenparcialApplication.class, args);
+	}
+
+}
---
+++ examenparcial/src/test/java/com/examenparcial/examenparcial/ExamenparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examenparcial.examenparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenparcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
