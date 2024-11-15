7363b78e12d777a5bcb25673713c09610dde8c55
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2024-11-14 20:24:53 -0500

    modificacion controller

--- examenparcial/src/main/java/com/examenparcial/examenparcial/controller/UsuarioController.java
+++ examenparcial/src/main/java/com/examenparcial/examenparcial/controller/UsuarioController.java
@@ -16,7 +16,7 @@ import com.examenparcial.examenparcial.model.UsuarioModel;
 import com.examenparcial.examenparcial.service.UsuarioService;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("usuario")
 public class UsuarioController {
 
     @Autowired
@@ -25,10 +25,23 @@ public class UsuarioController {
     @GetMapping("/all")
     public List<UsuarioModel> getAll() {
         return usuarioService.findAll();
+    }
 
-}
-
-@PostMapping("/create")
-public UsuarioModel create(UsuarioModel model) {
-
+    @PostMapping("/create")
+    public UsuarioModel create(@RequestBody UsuarioModel model) {
+        return usuarioService.add(model);
+    }
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable("id") int id) {
+        return usuarioService.delete(id) ? "deleted" : "not deleted";
+    }
+    @PutMapping("/update/{id}")
+    public UsuarioModel update(@RequestBody UsuarioModel model, @PathVariable("id") int id) {
+        
+        return usuarioService.update(model, id);
+    }
+    @GetMapping("/{id}")
+    public UsuarioModel findById(@PathVariable("id") int id) {
+        return usuarioService.findById(id);
+    }
 }
