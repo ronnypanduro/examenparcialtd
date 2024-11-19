8c0180766d5a62fb8cfa8b1eb491daa8c681ad87
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2024-11-18 21:47:09 -0500

    ruta centralizada

--- examenparcial/src/main/java/com/examenparcial/examenparcial/controller/AlumnoController.java
+++ examenparcial/src/main/java/com/examenparcial/examenparcial/controller/AlumnoController.java
@@ -17,32 +17,32 @@ import com.examenparcial.examenparcial.service.AlumnoService;
 
 
 @RestController
-@RequestMapping("rest/alumno")
+@RequestMapping(RutaApi.RUTABASE)
 public class AlumnoController {
 
     @Autowired
     AlumnoService alumnoService;
 
-    @GetMapping("/all")
+    @GetMapping(RutaApi.RUTA_GETALUMNOS)
     public List<AlumnoModel> getAll() {
         return alumnoService.getAlumnos();
     }
 
-    @PostMapping("/create")
+    @PostMapping(RutaApi.RUTA_POSTALUMNO)
     public AlumnoModel create(@RequestBody AlumnoModel model) {
         return alumnoService.guardarAlumno(model);
     }
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping(RutaApi.RUTA_DELETEALUMNO)
     public String delete(@PathVariable("id") int id) {
         return alumnoService.eliminarAlumno(id) ? "deleted" : "not exste id";
     }
-    @PutMapping("/update/{id}")
+    @PutMapping(RutaApi.RUTA_PUTALUMNO)
     public AlumnoModel update(@RequestBody AlumnoModel model, @PathVariable("id") int id) {
         AlumnoModel alumno = new AlumnoModel(); 
         alumno.setId(id); alumno.setNombre(model.getNombre()); alumno.setNota(model.getNota());
         return alumnoService.actualizarAlumno(alumno);
     }
-    @GetMapping("/{id}")
+    @GetMapping(RutaApi.RUTA_GETALUMNO)
     public AlumnoModel findById(@PathVariable("id") int id) {
         
         return alumnoService.getAlumnoId(id);
---
+++ examenparcial/src/main/java/com/examenparcial/examenparcial/controller/RutaApi.java
@@ -0,0 +1,10 @@
+package com.examenparcial.examenparcial.controller;
+
+public class RutaApi {
+    public static final String RUTABASE = "restapi/alumnos";
+    public static final String RUTA_GETALUMNO = "/{id}";
+    public static final String RUTA_GETALUMNOS = "/all";
+    public static final String RUTA_POSTALUMNO = "/create";
+    public static final String RUTA_DELETEALUMNO = "/delete/{id}";
+    public static final String RUTA_PUTALUMNO = "/update/{id}";
+}
